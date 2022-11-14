43f187eff26198963e55d43bba7ec672a673442e
edgarhernandez13 <alex.hernandez@sryas.com>
2022-11-14 16:40:59 -0600

    first version

---
+++ src/components/rickAndMorty.vue
@@ -0,0 +1,114 @@
+<template>
+    <div id="all">
+        <h1>Rick And Morty</h1>
+        <div id="characters">
+            <div class="container"  v-for="character in characters.results " :key = "character.id">
+                <img :src="character.image" :alt="character.name">
+                <div class="info">
+                    <a href="">{{character.name}}</a>
+                    <span id="status" :class="character.status"></span>
+                    <p> Status:</p>
+                    <span>{{character.status}}</span>
+                    <p>Last known location:</p>
+                    <SPan>{{character.location.name}}</SPan>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    namme: "rickAndMorty",
+    data() {
+        return {
+            characters:[]
+        }
+    },
+    methods: {
+        callingAPI (){
+            fetch("https://rickandmortyapi.com/api/character").then(res => res.json().then((data)=>{
+                this.characters = data
+            }))
+        }
+    },
+    mounted() {
+        this.callingAPI()
+    },
+}
+</script>
+
+
+<style scoped>
+
+.container {
+    display: flex;
+    margin: 5% 5% 0% 5%;
+    height: auto;
+    width: 525px;
+    border: black 2px solid;
+    border-radius: 20px;
+    background-color: #54b534;
+}
+
+.container:hover{
+    background-color: #41ff01 20px solid;
+}
+
+img{
+    display: flex;
+    height: 250px;
+    width: 220;
+    border-radius: 20px;
+}
+
+.info{
+    width: 300px;
+    color: white;
+    display: block;
+    justify-content: center;
+    align-items: center;
+}
+
+#characters{
+    display: flex;
+    flex-flow: row wrap;
+    width: auto;
+    /* border: black 2px solid; */
+    justify-content: space-around;
+    background-color: transparent;
+    margin: 0%;
+    padding: 0%;
+}
+
+#all{
+    background-color: rgb(57, 55, 90);
+}
+
+h1{
+    color: white;
+    background-color: transparent;
+}
+
+a{
+    font-size: 30px;
+}
+
+#status{
+    height: 10px;
+    width: 10px;
+    border-radius: 50%;
+    display: inline-block;
+}
+
+
+.Alive{
+    background-color: green;
+}
+
+.Dead{
+    background-color: red;
+}
+.unknow{
+    background-color: white;
+}
+</style>
---
+++ src/components/userName.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+        <h1>"My name"</h1>
+        <input type="text" v-model="input">
+        <ul>
+            <li v-for="(name) in filteredNames" v-bind:key = "name">
+                {{index}} {{name}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name :"userName",
+    data(){
+        return{
+            names: [
+                'Parth',
+                'Eduard',
+                'Frank',
+                'Edger',
+                'Max',
+                'Chencho',
+                'Demian',
+                'Alexis',
+                'Tali',
+                'Luz',
+                'Juan'
+            ],
+            input:''
+        }
+    },
+    computed: {
+        filteredNames() {
+            return this.input !== '' ? this.names.filter(name => name.toLowerCase().includes(this.input.toLowerCase())) : ''
+        },
+    }
+}
+</script>
+
+<style scoped>
+template{
+    display: flex;
+}
+
+div{
+    width: 99%;
+    height: 500px;
+    background-color: rgb(180, 180, 180);
+    margin: 0%;
+    border: 5px solid black;
+
+}
+
+h1{
+    width: 100%;
+    text-align: center;
+    font-size: 70px;
+    background-color: rgb(73, 187, 73);
+    margin-top: 0;
+}
+
+input{
+    width: 50%;
+    height: 50px;
+    border: 3px solid rgb(38, 114, 158);
+    font-size: 40px;
+    text-align: center;
+    outline: 5px solid white;
+    border-radius: 2px;
+}
+
+ul li{
+    font-size: 35px;
+    height: 40px;
+    width: 35%;
+    background-color: rgb(73, 187, 73);
+    margin: 0 auto;
+    /* align-items: center; */
+}
+</style>
